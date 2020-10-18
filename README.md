e777926e4adfce4b9d5251f7564909530357dee8
fumuling <51187109+fumuling@users.noreply.github.com>
2020-10-18 14:26:22 +0800

    第一次初始化

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ancientModernProseClassifier
 现代文、文言文、诗、词分类
+
+python 3.8.5
---
+++ main.py
@@ -0,0 +1,8 @@
+def welcome():
+    print("hello niave bayes!")
+
+def main():
+    welcome()
+
+if __name__ == "__main__":
+    main()
