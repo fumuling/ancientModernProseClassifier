24f78c391ec9d9177c3eb16d4ad620cbd1143349
fumuling <51187109+fumuling@users.noreply.github.com>
2020-10-25 11:25:32 +0800

    分词GUI完成

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # ancientModernProseClassifier
-现代文、文言文、诗、词分类
+基于朴素贝叶斯和TF-IDF的文本分类课程小作业，类别为现代文、文言文、诗、词。
 
-python 3.8.5
+先运行trainModel.ipynb生成模型并保存，再运行main.py，就可以输入文本进行类型的分类了。
+
+python 3.8.3
+
+jieba
+
+sklearn
+
+naive bayes
+
--- main.py
+++ main.py
@@ -1,8 +1,71 @@
-def welcome():
-    print("hello niave bayes!")
+import tkinter
+import tkinter.messagebox
+import joblib
+import sys
+import jieba
+from sklearn.feature_extraction.text import TfidfVectorizer
+LABEL_MAP = ['现代文', '文言文', '诗', '词']
 
-def main():
-    welcome()
+window = tkinter.Tk()
+window.title("文本分类器")
+window.geometry("600x400")
+l1 = tkinter.Label(window, text = "请输入要分类的文本:")
+l1.pack()
+e = tkinter.Entry(window, show = None)
+e.pack()
 
-if __name__ == "__main__":
-    main()
+with open('characters-master/stop_words.txt', encoding='utf-8') as f:
+    stop_words = [line.strip() for line in f.readlines()]
+clf1 = joblib.load("MultinomialNB_clf_model.pkl")
+clf2 = joblib.load("BernoulliNB_clf_model.pkl")
+train_tfidf_vector = joblib.load("TF-IDF_model.pkl")
+
+
+def MultinomialNB():
+    var = e.get()
+    word_list = jieba.lcut(var)
+    documents = []
+    words = [wl for wl in word_list if wl not in stop_words]
+    sentence = ' '.join(words)
+    documents.append(sentence)
+    test_tfidf_vector = TfidfVectorizer(max_df=0.5, vocabulary=train_tfidf_vector.vocabulary_)
+    test_x = test_tfidf_vector.fit_transform(documents)
+    res = clf1.predict(test_x)
+    ans = LABEL_MAP[res[0]]
+    tkinter.messagebox.showinfo('提示', ans)
+
+
+def BernoulliNB():
+    var = e.get()
+    word_list = jieba.lcut(var)
+    documents = []
+    words = [wl for wl in word_list if wl not in stop_words]
+
+    sentence = ' '.join(words)
+    documents.append(sentence)
+    test_tfidf_vector = TfidfVectorizer(max_df=0.5, vocabulary=train_tfidf_vector.vocabulary_)
+    test_x = test_tfidf_vector.fit_transform(documents)
+    res = clf2.predict(test_x)
+    ans = LABEL_MAP[res[0]]
+    tkinter.messagebox.showinfo('提示', ans)
+
+def on_closing():
+    if tkinter.messagebox.askokcancel("Quit","现在就退出么"):
+        window.destroy()
+
+btn1 = tkinter.Button(window, text="用多项式朴素贝叶斯分类", command = MultinomialNB)
+btn2 = tkinter.Button(window, text="用伯努利朴素贝叶斯分类", command = BernoulliNB)
+
+
+
+btn1.pack()
+btn2.pack()
+
+
+
+window.protocol("WM_DELETE_WINDOW", on_closing)
+
+
+window.mainloop()
+
+sys.exit()
